41b366ff336ff80f83f6991c3026cfcacba69579
Charles <charles@medeo-health.com>
2020-09-20 12:25:27 +0200

    reduce hero size

--- Results/components/LeaderBoardHero.tsx
+++ Results/components/LeaderBoardHero.tsx
@@ -10,8 +10,8 @@ const LeaderBoardHero = ({participant}) => (
     style={tailwind(
       'p-4 mb-4 bg-white rounded-lg justify-between items-center',
     )}>
-    <View style={tailwind('bg-red-400 p-8 rounded-full mb-4')}>
-      <FontAwesomeIcon icon={faTrophy} color={getColor('red-600')} size={64} />
+    <View style={tailwind('bg-red-200 p-6 rounded-full mb-4')}>
+      <FontAwesomeIcon icon={faTrophy} color={getColor('red-600')} size={32} />
     </View>
     <Text style={tailwind('font-bold mb-4')}>{participant.email}</Text>
     <Text style={tailwind('font-bold text-red-800 text-xl')}>
